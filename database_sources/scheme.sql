f178e2e0470dc7f24f8c4f1f2f52acbb985b7b72
Яремко Роман <salvoronis@gmail.com>
2020-11-08 19:23:22 +0300

    little fix & std

--- database_sources/scheme.sql
+++ database_sources/scheme.sql
@@ -1,116 +1,116 @@
 create table Hidden_Village (
     village_ID serial primary key,
-    name varchar(100),
+    name varchar(100) not null,
 );
 
 create table Ninja (
     ninja_ID serial primary key,
-    name varchar(100),
+    name varchar(100) not null,
     age integer,
-    sex varchar(1),
-    village integer references Hidden_Village(village_ID),
-    clan integer references Clan(clan_ID),
+    sex varchar(1) not null,
+    village integer references Hidden_Village(village_ID) on delete cascade,
+    clan integer references Clan(clan_ID) on delete cascade,
     status varchar(10)
 );
 
 create table Destroyed_village (
-    village_ID integer references Hidden_Village(village_ID),
-    destroyer integer references Ninja(ninja_ID),
+    village_ID integer references Hidden_Village(village_ID) on delete cascade,
+    destroyer integer references Ninja(ninja_ID) on delete cascade,
     quantity integer
 );
 
 create table Country (
     country_ID serial primary key,
-    name varchar(100),
-    country_lord varchar(100),
-    hidden_village integer references Hidden_Village(village_ID)
+    name varchar(100) not null,
+    country_lord varchar(100) not null,
+    hidden_village integer references Hidden_Village(village_ID) on delete cascade
 );
 
 create table Country_lord (
     lord_ID serial primary key,
-    name varchar(100),
-    age integer,
+    name varchar(100) not null,
+    age integer not null,
     sex varchar(1),
     status varchar(10)
 );
 
 create table Citizen (
     citizen_ID serial primary key,
-    village integer references Hidden_Village(village_ID),
-    name varchar(100),
+    village integer references Hidden_Village(village_ID) on delete cascade,
+    name varchar(100) not null,
     age integer,
     sex varchar(1),
     status varchar(10)
 );
 
 create table Parents (
-    children_ID integer references Citizen(citizen_ID),
-    parent_Id integer references Citizen(citizen_ID)
+    children_ID integer references Citizen(citizen_ID) on delete cascade,
+    parent_Id integer references Citizen(citizen_ID) on delete cascade
 );
 
 create table Clan (
     clan_ID serial primary key,
-    name varchar(100),
-    leader integer references Ninja(ninja_ID),
-    village integer references Hidden_Village(village_ID),
-    prestige integer
+    name varchar(100) not null,
+    leader integer references Ninja(ninja_ID) on delete cascade,
+    village integer references Hidden_Village(village_ID) on delete cascade,
+    prestige integer not null
 );
 
 create table Ninja_parents (
-    children_ID integer references Ninja(ninja_ID),
-    parent_ID integer references Ninja(ninja_ID)
+    children_ID integer references Ninja(ninja_ID) on delete cascade,
+    parent_ID integer references Ninja(ninja_ID) on delete cascade
 );
 
 create table Biju (
     biju_Id serial primary key,
-    name varchar(100),
-    count_of_tails integer,
-    jinchuriki integer references Ninja(ninja_ID)
+    name varchar(100) not null,
+    count_of_tails integer not null,
+    jinchuriki integer references Ninja(ninja_ID) on delete cascade
 );
 
 create table Ninjas_rank (
     rank_ID serial primary key,
-    name varchar(100),
+    name varchar(100) not null,
     condition_of_receipt varchar(100)
 );
 
 create table Ranked_ninja (
-    rank_ID integer references Ninjas_rank(rank_ID),
-    ninja_ID integer references Ninja(ninja_ID)
+    rank_ID integer references Ninjas_rank(rank_ID) on delete cascade,
+    ninja_ID integer references Ninja(ninja_ID) on delete cascade
 );
 
 create table Type (
     type_ID serial primary key,
-    name varchar(100)
+    name varchar(100) not null
 );
 
 create table Additional_type (
     addtype_ID serial primary key,
-    name varchar(100)
+    name varchar(100) not null
 );
 
 create table Technic_rank (
     techrank_ID serial primary key,
-    name varchar(20)
+    name varchar(20) not null
 );
 
 create table Technic (
     technic_ID serial primary key,
-    type integer references Type(type_ID),
-    additional_type integer references Additional_type(addtype_ID),
-    blood_restriction Boolean,
-    rank integer references Technic_rank(techrank_ID)
+    type integer references Type(type_ID) on delete cascade,
+    additional_type integer references Additional_type(addtype_ID) on delete cascade,
+    blood_restriction Boolean not null,
+    rank integer references Technic_rank(techrank_ID) on delete cascade
     rune_seals varchar(255)
 );
 
 create table Ninja_technic (
-    ninja_ID integer references Ninja(ninja_ID),
-    technic_ID integer references Technic(technic_ID)
+    ninja_ID integer references Ninja(ninja_ID) on delete cascade,
+    technic_ID integer references Technic(technic_ID) on delete cascade
 );
 
 create table War (
     war_ID serial primary key,
-    name varchar(100),
+    name varchar(100) not null,
     attacking_country integer references Country(country_ID),
     defending_country integer references Country(country_ID),
     loss_of_attackers integer,
@@ -119,9 +119,9 @@ create table War (
 
 create table Battle (
     battle_ID serial primary key,
-    war integer references War(war_ID),
-    territory integer references Country(country_ID),
+    war integer references War(war_ID) on delete cascade,
+    territory integer references Country(country_ID) on delete cascade,
     loss integer,
     duration integer,
-    name varchar(100)
+    name varchar(100) not null
 );
