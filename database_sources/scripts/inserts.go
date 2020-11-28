6600de0df5cae226ede82e2cfab20ce318b9d6de
salvoronis <salvoronis@gmail.com>
2020-11-28 21:45:04 +0300

    code style

--- database_sources/scripts/inserts.go
+++ database_sources/scripts/inserts.go
@@ -35,30 +35,120 @@ func main() {
 	if err != nil {panic(err)}
 
 	vil_count := hid_vil_gen(inserts, template)
-	_ = clan_gen(inserts, template, vil_count)
+	clan_count := clan_gen(inserts, template, vil_count)
+	ninja_count := ninja_gen(inserts, template, vil_count, clan_count)
+	Clan_leader_gen(inserts, template, clan_count, ninja_count)
 }
 
 func hid_vil_gen(inserts *os.File, hidden_vil_tmp *template.Template) int {
-	test_data := Request{Table_name: "Hidden_Village", Fields: []string{"name"}}
+	test_data := Request{
+		Table_name: "Hidden_Village",
+		Fields: []string{
+			"name",
+		},
+	}
 	for i := 0; i < 10; i++ {
 		test_data.Data = append(test_data.Data, "('" + generate_rand_str(2+rand.Intn(3)) + "'),")
 	}
 	test_data.Data = append(test_data.Data, "('" + generate_rand_str(2+rand.Intn(3)) + "');")
 
-	err := hidden_vil_tmp.Execute(inserts, test_data)
-	if err != nil {panic(err)}
+	Write_data(inserts, hidden_vil_tmp, &test_data)
 	return len(test_data.Data)
 }
 
 func clan_gen(inserts *os.File, tmp *template.Template, vil_id int) int {
-	clans := Request{Table_name: "Clan", Fields: []string{"name","village","prestige"}}
-	for i := 0; i < 10; i++ {
-		clans.Data = append(clans.Data, "('"+ generate_rand_str(3+rand.Intn(3)) + "'," + strconv.Itoa(1+rand.Intn(vil_id-1)) + "," + strconv.Itoa(rand.Intn(100)) + "),")
+	clans := Request{
+		Table_name: "Clan",
+		Fields: []string{
+			"name,",
+			"village,",
+			"prestige",
+		},
 	}
-	clans.Data = append(clans.Data, "('"+ generate_rand_str(3+rand.Intn(3)) + "'," + strconv.Itoa(rand.Intn(vil_id)) + "," + strconv.Itoa(rand.Intn(100)) + ");")
-	err := tmp.Execute(inserts, clans)
-	if err != nil {panic(err)}
+	for i := 0; i < 10; i++ {
+		clans.Data = append(clans.Data, "('"+ generate_rand_str(3+rand.Intn(3)) + "'," +
+			strconv.Itoa(1+rand.Intn(vil_id-1)) + "," +
+			strconv.Itoa(rand.Intn(100)) + "),")
+	}
+	clans.Data = append(clans.Data, "('"+ generate_rand_str(3+rand.Intn(3)) + "'," +
+		strconv.Itoa(rand.Intn(vil_id)) + "," +
+		strconv.Itoa(rand.Intn(100)) + ");")
+
+	Write_data(inserts, tmp, &clans)
 	return len(clans.Data)
+}
+
+func ninja_gen(inserts *os.File, tmp *template.Template, vil_id, clan_id int) int {
+	ninjas := Request{
+		Table_name: "Ninja",
+		Fields: []string{
+			"name,",
+			"age,",
+			"sex,",
+			"village,",
+			"clan,",
+			"status",
+		},
+	}
+	for i := 0; i < 10; i++ {
+		var sex string = "M"
+		var status string = "alive"
+		if rand.Intn(2) == 1 {
+			sex = "M"
+		} else {
+			sex = "F"
+		}
+		if rand.Intn(2) == 1 {
+			status = "alive"
+		} else {
+			status = "dead"
+		}
+
+		ninjas.Data = append(ninjas.Data, "('"+
+			generate_rand_str(2+rand.Intn(2)) + " " + generate_rand_str(2+rand.Intn(2))+"'," +
+			strconv.Itoa(rand.Intn(100)) + ",'" +
+			sex + "'," +
+			strconv.Itoa(1+rand.Intn(vil_id-1)) + "," +
+			strconv.Itoa(1+rand.Intn(clan_id-1)) + ",'" +
+			status + "'),")
+	}
+	ninjas.Data = append(ninjas.Data, "('"+
+		generate_rand_str(2+rand.Intn(2)) + " " + generate_rand_str(2+rand.Intn(2))+"'," +
+		strconv.Itoa(rand.Intn(100)) + ",'" +
+		"F"+"'," +
+		strconv.Itoa(1+rand.Intn(vil_id-1)) + "," +
+		strconv.Itoa(1+rand.Intn(clan_id-1)) + ",'" +
+		"dead" + "');")
+
+	Write_data(inserts, tmp, &ninjas)
+	return len(ninjas.Data)
+}
+
+func Clan_leader_gen(inserts *os.File, tmp *template.Template, clan_count, ninja_count int) {
+	request := Request{
+		Table_name: "Clan_leader",
+		Fields: []string{
+			"clan_ID,",
+			"ninja_ID",
+		},
+	}
+	for i := 1; i < clan_count; i++ {
+		request.Data = append(request.Data, "(" +
+			strconv.Itoa(i) + "," +
+			strconv.Itoa(1+rand.Intn(ninja_count-1)) + "),")
+	}
+	request.Data = append(request.Data, "(" +
+		strconv.Itoa(clan_count) + "," +
+		strconv.Itoa(1+rand.Intn(ninja_count-1)) + ");")
+
+	Write_data(inserts, tmp, &request)
+}
+
+func Write_data(inserts *os.File, tmp *template.Template, data *Request){
+	err := tmp.Execute(inserts, data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func generate_rand_str(size int) string {
--- database_sources/scripts/inserts.sql
+++ database_sources/scripts/inserts.sql
@@ -1,26 +1,52 @@
 insert into Hidden_Village(name) values
-	('yasureri'),
-	('weteachi'),
-	('yumore'),
-	('miseresa'),
-	('arosu'),
-	('sune'),
-	('emuwoyu'),
-	('yani'),
-	('rorareko'),
-	('metaeha'),
-	('teko');
+	('kiemuyu'),
+	('remake'),
+	('iwa'),
+	('ihi'),
+	('soshi'),
+	('mihahi'),
+	('hisea'),
+	('urorima'),
+	('reseshiho'),
+	('shiwinine'),
+	('ruyunu');
 
-insert into Clan(namevillageprestige) values
-	('wiwohewene',5,91),
-	('yamimi',9,42),
-	('moomo',9,11),
-	('suhotofume',7,94),
-	('nuwatsuro',7,79),
-	('ritetsu',5,82),
-	('shiwiyu',8,10),
-	('neeho',7,1),
-	('chisotoshi',7,83),
-	('tanii',8,24),
-	('wonimoke',7,17);
+insert into Clan(name,village,prestige) values
+	('shisechimi',7,97),
+	('aneoe',10,59),
+	('tekesetsuta',8,44),
+	('ruimashi',5,21),
+	('mohiwakoe',4,48),
+	('noehi',4,97),
+	('hemukomase',6,61),
+	('hiratsu',2,21),
+	('yaruku',8,76),
+	('nimekiwe',2,2),
+	('tenisurete',0,20);
+
+insert into Ninja(name,age,sex,village,clan,status) values
+	('woya rusa',51,'M',8,3,'dead'),
+	('ine mishi',17,'M',8,9,'dead'),
+	('wahiya yanuke',42,'F',10,8,'alive'),
+	('nutsu ake',83,'M',6,8,'dead'),
+	('sume hotana',23,'F',10,1,'dead'),
+	('kuwi ekore',35,'F',10,8,'dead'),
+	('haruu rawi',8,'M',5,9,'alive'),
+	('tea yaromu',49,'F',2,4,'dead'),
+	('neyosu tsurio',56,'M',4,9,'alive'),
+	('mitoka muatsu',89,'F',6,1,'dead'),
+	('wayue wechi',17,'F',4,9,'dead');
+
+insert into Clan_leader(clan_ID,ninja_ID) values
+	(1,4),
+	(2,5),
+	(3,4),
+	(4,2),
+	(5,9),
+	(6,4),
+	(7,9),
+	(8,9),
+	(9,8),
+	(10,9),
+	(11,5);
 
