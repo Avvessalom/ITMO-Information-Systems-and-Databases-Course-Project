05258899398240de64cecc1f1b75bb61fb340de5
salvoronis <salvoronis@gmail.com>
2020-11-28 18:44:18 +0300

    iroha strings

--- database_sources/scripts/inserts.go
+++ database_sources/scripts/inserts.go
@@ -6,47 +6,66 @@ import(
 	"math/rand"
 	"time"
 	"strings"
+	"strconv"
 )
 
-type Hidden_Village struct {
-	Name []string
+type Request struct{
+	Table_name string;
+	Fields, Data []string
 }
-
-var hidden_village string = `insert into Hidden_Village(name) values{{range .Name}}
-	{{.}}{{end}}
-`
+var (
+	template_str string = "insert into {{.Table_name}}({{range .Fields}}{{.}}{{end}}) values{{range .Data}}\n\t{{.}}{{end}}\n\n"
+	iroha []string = []string{
+		"i", "ro", "ha", "ni", "ho", "he", "to",
+		"chi", "ri", "nu", "ru", "wo",
+		"wa", "ka", "yo", "ta", "re", "so",
+		"tsu", "ne", "na", "ra", "mu",
+		"u", "wi", "no", "o", "ku", "ya", "ma",
+		"ke", "fu", "ko", "e", "te",
+		"a", "sa", "ki", "yu", "me", "mi", "shi",
+		"we", "hi", "mo", "se", "su",
+	}
+)
 
 func main() {
+	_ = os.Remove("./inserts.sql")
 	inserts, err := os.OpenFile("./inserts.sql",os.O_RDWR|os.O_CREATE, 0777)
-	if err != nil {panic (err)}
-	hid_vil_gen(inserts);
+	if err != nil {panic(err)}
+	template, err := template.New("Request").Parse(template_str)
+	if err != nil {panic(err)}
+
+	vil_count := hid_vil_gen(inserts, template)
+	_ = clan_gen(inserts, template, vil_count)
 }
 
-func hid_vil_gen(inserts *os.File) {
-	/*test := Hidden_Village{
-		Name: []string{
-			"Konoha",
-			"Rain",
-		},
-	}*/
-	//test_data := make([]string,0);
-	test_data := Hidden_Village{}
+func hid_vil_gen(inserts *os.File, hidden_vil_tmp *template.Template) int {
+	test_data := Request{Table_name: "Hidden_Village", Fields: []string{"name"}}
 	for i := 0; i < 10; i++ {
-		test_data.Name = append(test_data.Name, "('" + generate_rand_str(8) + "'),")
+		test_data.Data = append(test_data.Data, "('" + generate_rand_str(2+rand.Intn(3)) + "'),")
 	}
-	test_data.Name = append(test_data.Name, "('" + generate_rand_str(8) + "');")
-	hidden_vil_tmp, err := template.New("Hidden_Village").Parse(hidden_village)
+	test_data.Data = append(test_data.Data, "('" + generate_rand_str(2+rand.Intn(3)) + "');")
+
+	err := hidden_vil_tmp.Execute(inserts, test_data)
 	if err != nil {panic(err)}
-	err = hidden_vil_tmp.Execute(inserts, test_data)
+	return len(test_data.Data)
+}
+
+func clan_gen(inserts *os.File, tmp *template.Template, vil_id int) int {
+	clans := Request{Table_name: "Clan", Fields: []string{"name","village","prestige"}}
+	for i := 0; i < 10; i++ {
+		clans.Data = append(clans.Data, "('"+ generate_rand_str(3+rand.Intn(3)) + "'," + strconv.Itoa(1+rand.Intn(vil_id-1)) + "," + strconv.Itoa(rand.Intn(100)) + "),")
+	}
+	clans.Data = append(clans.Data, "('"+ generate_rand_str(3+rand.Intn(3)) + "'," + strconv.Itoa(rand.Intn(vil_id)) + "," + strconv.Itoa(rand.Intn(100)) + ");")
+	err := tmp.Execute(inserts, clans)
 	if err != nil {panic(err)}
+	return len(clans.Data)
 }
 
 func generate_rand_str(size int) string {
 	rand.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz")
 	var b strings.Builder
 	for i := 0; i < size; i++ {
-		b.WriteRune(chars[rand.Intn(len(chars))])
+		b.WriteString(iroha[rand.Intn(len(iroha))])
 	}
 	return b.String()
 }
--- database_sources/scripts/inserts.sql
+++ database_sources/scripts/inserts.sql
@@ -1,12 +1,26 @@
 insert into Hidden_Village(name) values
-	('AEayHuba'),
-	('UoToqinH'),
-	('AEAkbRxB'),
-	('qOlxLgxj'),
-	('sZJQpvWg'),
-	('QViHkFsK'),
-	('eTUgQMFg'),
-	('eisXOPSP'),
-	('wkCAFhsi'),
-	('PtkctFoX'),
-	('UBeDsXse');
+	('yasureri'),
+	('weteachi'),
+	('yumore'),
+	('miseresa'),
+	('arosu'),
+	('sune'),
+	('emuwoyu'),
+	('yani'),
+	('rorareko'),
+	('metaeha'),
+	('teko');
+
+insert into Clan(namevillageprestige) values
+	('wiwohewene',5,91),
+	('yamimi',9,42),
+	('moomo',9,11),
+	('suhotofume',7,94),
+	('nuwatsuro',7,79),
+	('ritetsu',5,82),
+	('shiwiyu',8,10),
+	('neeho',7,1),
+	('chisotoshi',7,83),
+	('tanii',8,24),
+	('wonimoke',7,17);
+
