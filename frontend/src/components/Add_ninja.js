497b6e06984f3b6266468de8daa68167bba37ec9
avvessalom <avvessalom@gmail.com>
2020-12-10 14:51:27 +0300

    added Ninja page

---
+++ frontend/src/components/Add_ninja.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Add_ninja = () => {
+    return (
+        <form>
+            <div>
+                <button className="btn btn-block btn-warning btn-lg" type="submit">Add new Ninja</button>
+            </div>
+        </form>
+    )
+}
+
--- frontend/src/pages/Ninjas.js
+++ frontend/src/pages/Ninjas.js
@@ -1,18 +1,59 @@
-import React, {Fragment} from "react";
-import {Search_form} from "../components/search_form";
-
+import React from "react";
+import Table from "react-bootstrap/Table"
+import {Add_ninja} from "../components/Add_ninja";
 
 export const Ninjas = () => {
     return (
         <header className="masthead text-center">
-            <div className="overlay home_back"/>
+            <div className="ninja-button"><Add_ninja /></div>
             <div className="container">
                 <div className="row">
                     <div className="col-xl-9 mx-auto">
-                        <h1 className="brand-heading">ninjas</h1>
+                        <h1 className="ninja-heading">ninjas</h1>
                     </div>
                     <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
-                        <Search_form />
+                        <Table striped bordered hover>
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Clan</th>
+                                <th>Age</th>
+                                <th>Sex</th>
+                                <th>Village</th>
+                                <th>Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Eugege</td>
+                                <td>Cool</td>
+                                <td>54</td>
+                                <td>M</td>
+                                <td>Saint P</td>
+                                <td>Alive</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Roma</td>
+                                <td>Lox</td>
+                                <td>0</td>
+                                <td>F</td>
+                                <td>Siberia</td>
+                                <td>Alive</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Vladimir</td>
+                                <td>Nikolaev</td>
+                                <td>~30</td>
+                                <td>M</td>
+                                <td>Saint P</td>
+                                <td>Alive</td>
+                            </tr>
+                            </tbody>
+                        </Table>
                     </div>
                 </div>
             </div>
