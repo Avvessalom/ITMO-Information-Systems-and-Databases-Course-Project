721a07c9dfbd82a2ffd8db10f2a8e26078be755a
avvessalom <avvessalom@gmail.com>
2020-12-30 13:07:39 +0300

    final version 'Add_ninja' button

--- frontend/src/components/Add_ninja.js
+++ frontend/src/components/Add_ninja.js
@@ -1,110 +1,7 @@
-import React, {useEffect, useState, useRef, Component} from "react";
+import React, {Component} from "react";
 import {Modal, FormGroup, Form, Button} from "react-bootstrap";
 import axios from "axios";
 
-const useComponentWillMount = func => {
-    const willMount = useRef(true);
-
-    if (willMount.current) {
-        func();
-    }
-    willMount.current = false;
-};
-
-function MyVerticallyCenteredModal(props) {
-    const [name, setName] = React.useState('');
-    const [clan, setClan] = React.useState();
-    const ninjas = useComponentWillMount(() => {
-        let responsee = [];
-        axios.get('http://localhost:8080/backend/naruto-api/stdquery/ninjas')
-            .then((response) => {
-                responsee = response.data
-            });
-        // let ninja = responsee.map((ninja) => {
-        //     return (
-        //         <option>{ninja.clan}</option>
-        //     )
-        // })
-        return responsee
-    })
-    console.log("aaaaaaaaaaaaaaaa",ninjas);
-
-    //  function addNinja() {
-    //     axios.post('http://localhost:8080/backend/naruto-api/stdquery/ninjas',name).then((response) => {
-    //         console.log(response.data);
-    //     })
-    // }
-    function addNinja() {
-        axios.post('http://localhost:3000/~s250643/ninjas', {"name": name, "clan": clan}).then((response) => {
-            console.log(response.data);
-        })
-    }
-
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Add new Ninja
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <h4>Enter ninja details</h4>
-                <Form.Group>
-                    <Form.Label> Ninja Name</Form.Label>
-                    <Form.Control type="text" placeholder="Normal text" id="name" onChange={event => setName(event.target.value)}  />
-                    <br />
-                    <Form.Label>Clan</Form.Label>
-                    <Form.Control as="select" onChange={event => setClan(event.target.value)}>
-                        <option>Default select</option>
-                        <option>alala</option>
-                        {ninjas}
-                    </Form.Control>
-                    <br />
-                    <Form.Label>Age</Form.Label>
-                    <Form.Control as="select">
-                        <option>0</option>
-                    </Form.Control>
-                    <br />
-                    <Form.Label>Sex</Form.Label>
-                    <Form.Control as="select">
-                        <option>M</option>
-                        <option>F</option>
-                    </Form.Control>
-                    <br />
-                    <Form.Label>Village</Form.Label>
-                    <Form.Control as="select">
-                        <option>Default select</option>
-                    </Form.Control>
-                    <br />
-                        <Form.Label as="legend" column sm={2}>
-                            Status
-                        </Form.Label>
-                            <Form.Check
-                                type="radio"
-                                label="alive"
-                                name="formHorizontalRadios"
-                                id="formHorizontalRadios1"
-                            />
-                            <Form.Check
-                                type="radio"
-                                label="dead"
-                                name="formHorizontalRadios"
-                                id="formHorizontalRadios2"
-                            />
-                </Form.Group>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button type="submit" onClick={addNinja}>Send</Button>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    );
-}
 class CenteredModal extends Component {
     constructor(props) {
         super(props);
@@ -118,31 +15,45 @@ class CenteredModal extends Component {
             age: '',
             sex: ''
         },
-        ninjas: []
+        ninjas: [],
+        clans: [],
+        villages: []
     }
     addNinja = () => {
-        axios.post('http://localhost:3000/~s250643/ninjas', this.state.newNinjaData).then((response) => {
+        axios.post('http://localhost:8080/narutopedia/ninja/addNew', this.state.newNinjaData).then((response) => {
             console.log(response.data)
         })
     }
     componentWillMount() {
-        axios.get('http://localhost:8080/backend/naruto-api/stdquery/ninjas')
+        axios.get('http://localhost:8080/narutopedia/ninja')
             .then((response) => {this.setState({
                 ninjas: response.data
+            })
+                .then(error => console.log(error))
+            });
+        axios.get('http://localhost:8080/narutopedia/clans')
+            .then((response) => {this.setState({
+                clans: response.data
+            })
+                .then(error => console.log(error))
+            });
+        axios.get('http://localhost:8080/narutopedia/villages')
+            .then((response) => {this.setState({
+                villages: response.data
             })
                 .then(error => console.log(error))
             });
     }
 
     render() {
-        let clan = this.state.ninjas.map((ninja) => {
+        let clan = this.state.clans.map((ninja) => {
             return (
-                <option>{ninja.clan}</option>
+                <option>{ninja.name}</option>
             )
         })
-        let village = this.state.ninjas.map((ninja) => {
+        let village = this.state.villages.map((ninja) => {
             return (
-                <option>{ninja.village}</option>
+                <option>{ninja.name}</option>
             )
         })
         return(
@@ -174,6 +85,7 @@ class CenteredModal extends Component {
                             newNinjaData.clan = event.target.value;
                             this.setState(newNinjaData);
                         })}>
+                            <option> </option>
                             {clan}
                         </Form.Control>
                         <br />
@@ -205,7 +117,7 @@ class CenteredModal extends Component {
                             newNinjaData.village = event.target.value;
                             this.setState(newNinjaData);
                         })}>
-                            <option>Default select</option>
+                            <option> </option>
                             {village}
                         </Form.Control>
                         <br />
@@ -225,8 +137,8 @@ class CenteredModal extends Component {
                     </Form.Group>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button type="submit" onClick={this.addNinja}>Send</Button>
                     <Button onClick={this.props.onHide}>Close</Button>
+                    <Button type="submit" onClick={this.addNinja}>Send</Button>
                 </Modal.Footer>
             </Modal>
         )
